162ccc879fef2e3412588d93b67eeca21946d29e
arfontalvo <armando.fontalvo@anu.edu.au>
2022-09-15 18:01:49 +1000

    Back to Tbool

--- tests/PostProcessing.py
+++ tests/PostProcessing.py
@@ -18,6 +18,46 @@ class PostProcess:
 		self.num_fp=num_fp
 		self.num_el=num_el
 
+	def Tbool(self): # OELT and RELT generations
+
+		print(self.casedir)
+
+		N=10  # for lammda, ecliptic longitude
+		M=24  # for omega
+
+		Lammda=np.linspace(-np.pi,np.pi,N+1)
+		Omega=np.linspace(-np.pi,np.pi,M+1)
+		sun=SunPosition()
+
+		for r,d in enumerate(self.dni):
+			Tbool=np.zeros((N+1,M+1))
+			for n in range(3,8):
+				for m in range(int(0.5*M)+1):
+					delta = 23.4556*np.sin(Lammda[n])
+					theta=sun.zenith(self.latitude, delta, Omega[m]/np.pi*180.)
+					phi=sun.azimuth(self.latitude, theta, delta, Omega[m]/np.pi*180.)
+					elevation=90.-theta
+					if elevation<=8.:
+						continue
+					try:
+						fileo = open('%s/flux_table_n%d_m%d_d%s'%(self.casedir,n,m,d),'rb')
+						data = pickle.load(fileo)
+						fileo.close()
+						Tbool[n,m] = abs(max(data['T_HC'][0][self.num_el-1],data['T_HC'][1][self.num_el-1])-data['T_out'])<1.
+					except:
+						continue
+
+				for m in range(int(0.5*M)+1,M+1):
+					Tbool[n,m]=Tbool[n,M-m]
+
+			for n in range(3):
+				Tbool[n,:]=Tbool[5-n,:]
+
+			for n in range(8,11):
+				Tbool[n,:]=Tbool[15-n,:]
+
+			np.savetxt('%s/Tbool_%s.csv'%(self.casedir,d), Tbool, fmt='%s', delimiter=',')
+
 	def get_fluxes(self): # OELT and RELT generation
 
 		print(yellow('	Getting fluxes'))
@@ -31,6 +71,7 @@ class PostProcess:
 		sun=SunPosition()
 
 		for r,d in enumerate(self.dni):
+			T = np.genfromtxt('%s/Tbool_%s.csv'%(self.casedir,d),delimiter=',')
 			for fp in range(self.num_fp):
 				print('	d,fp: ({0:.2f},{1:g})'.format(d,fp))
 				f = open('%s/FLUX_fp%s_d%s.motab'%(self.casedir,fp+1,d),'w+')
@@ -76,7 +117,10 @@ class PostProcess:
 							elif j==0:
 								f.write('%.1f '%F_output[i,j])
 							else:
-								f.write('%.1f '%F_output[i,j])
+								if T[i-1,j-1]:
+									f.write('%.1f '%F_output[i,j])
+								else:
+									f.write('0.0 ')
 						f.write('\n')
 					f.write('\n')
 				f.close()
@@ -98,6 +142,7 @@ class PostProcess:
 			f.write('#1\n')
 			for r,d in enumerate(self.dni):
 				print('	d,fp: ({0:.2f},{1:g})'.format(d,fp))
+				T = np.genfromtxt('%s/Tbool_%s.csv'%(self.casedir,d),delimiter=',')
 				F[:,:,:]=0.
 				for n in range(3,8):
 					for m in range(int(0.5*M)+1):
@@ -138,7 +183,10 @@ class PostProcess:
 						elif j==0:
 							f.write('%.1f '%F_output[i,j])
 						else:
-							f.write('%s '%F_output[i,j])
+							if T[i-1,j-1]:
+								f.write('%s '%F_output[i,j])
+							else:
+								f.write('0.0 ')
 					f.write('\n')
 				f.write('\n')
 			f.close()
@@ -157,6 +205,7 @@ class PostProcess:
 		for r,d in enumerate(self.dni):
 			E = np.genfromtxt('%s/F_optic_%s.csv'%(self.casedir,d),delimiter=',')
 			F = np.genfromtxt('%s/F_unavail_%s.csv'%(self.casedir,d),delimiter=',')
+			T = np.genfromtxt('%s/Tbool_%s.csv'%(self.casedir,d),delimiter=',')
 
 			f.write('double optics_%d(%d,%d)\n'%(r,E.shape[0],E.shape[1]))
 			g.write('double optics_%d(%d,%d)\n'%(r,E.shape[0],E.shape[1]))
@@ -169,8 +218,12 @@ class PostProcess:
 						f.write('%.1f '%E[i,j])
 						g.write('%.1f '%F[i,j])
 					else:
-						f.write('%s '%E[i,j])
-						g.write('%s '%F[i,j])
+						if T[i-1,j-1]:
+							f.write('%s '%E[i,j])
+							g.write('%s '%F[i,j])
+						else:
+							f.write('0.0 ')
+							g.write('0.0 ')
 				f.write('\n')
 				g.write('\n')
 			f.write('\n')
@@ -178,4 +231,17 @@ class PostProcess:
 
 		f.close()
 		g.close()
+
+if __name__=='__main__':
+	parent=os.path.join(os.getenv('HOME'),'ownCloud/phd_update/damage')
+	for fn in ['N06230','N08810']:
+		os.chdir(os.path.join(parent,fn))
+		fnFolder= [name for name in os.listdir('.') if os.path.isdir(name)]
+		for fnf in fnFolder:
+			casedir=os.path.join(os.getcwd(),fnf)
+			res=PostProcess([1.00,1.39,0.87,0.56],num_fp=2,num_el=450,casedir=casedir,latitude=37.56)
+			res.Tbool()
+			res.get_fluxes()
+			res.get_mflow()
+			res.Tables()
 
