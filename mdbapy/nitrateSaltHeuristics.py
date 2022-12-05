12ea5904ba77128988a60125ba5262a462af980e
arfontalvo <armando.fontalvo@anu.edu.au>
2022-12-05 19:12:30 +1100

    Detailed CS-VF all surfaces

--- mdbapy/nitrateSaltHeuristics.py
+++ mdbapy/nitrateSaltHeuristics.py
@@ -206,8 +206,8 @@ class receiver_cyl:
 		self.nz = nz
 		self.nt = nt
 		self.R_fouling = R_fouling
-		self.ab = ab
-		self.em = em
+		self.ab = ab#/(2./np.pi*(1.-ab)+ab)
+		self.em = em#/(2./np.pi*(1.-em)+em)
 		self.kp = kp
 		self.H_rec = H_rec
 		self.D_rec = D_rec
@@ -224,9 +224,10 @@ class receiver_cyl:
 		self.area = 0.25 * np.pi * pow(self.d,2.)    # Tube flow area (m2)
 		self.ln = np.log(Ro/Ri)                      # Log of Ro/Ri simplification
 		#Auxiliary variables
-		theta = np.linspace(0.0, np.pi, self.nt)
-		cosines = np.cos(theta)
-		self.cosines = np.maximum(cosines, np.zeros(nt))
+		self.thetas=np.linspace(0.0,np.pi,self.nt)
+		self.cosines = 1/self.dt*(np.sin(self.thetas+self.dt/2.)-np.sin(self.thetas-self.dt/2.))
+		neg = np.where(self.thetas>=np.pi/2)[0]
+		self.cosines[neg]=0.0
 		self.theta = np.linspace(-np.pi, np.pi,self.nt*2-2)
 		self.n = 3
 		self.l = alpha
@@ -253,19 +254,138 @@ class receiver_cyl:
 		for i in pdata:
 			T.append(float(pdata[i]['T']))
 		self.T_max_fatigue = max(T)
-		# view factors
-		zeros = np.where(theta>=0.5*np.pi)
-		alpha = 0.5*np.pi-theta
+		# Simplified effective em and ab
+		alpha = 0.5*np.pi-self.thetas
 		h = np.sqrt(self.Ro**2+4*self.Ro**2-4*self.Ro**2*np.cos(alpha))
 		beta = 0.5*np.pi-np.arcsin(self.Ro*np.sin(alpha)/h)
 		d = np.sqrt(self.Ro**2+h**2-2*self.Ro*h*np.cos(beta))
 		F = (d[0:nt-1]-d[1:]+self.Ro*self.dt)/(2*Ro*self.dt)
 		F = np.append(F,0)
-		F[zeros]=0.0
+		F[neg]=0.0
 		self.eff_em = em*F/(F*(1-em)+em)
 		self.eff_em = np.maximum(self.eff_em,1e-8)
 		self.eff_ab = ab*F/(F*(1-ab)+ab)
 		self.eff_ab = np.maximum(self.eff_ab,1e-8)
+		# View factors
+		self.F = self.factors()
+		C1=1/self.ab*np.eye(self.nt+1);C1[0,0]=1.0
+		C2=(1/self.ab-1)*self.F;C2[:,0]=0.0
+		C=C1-C2
+		self.Cinv=np.linalg.inv(C)
+
+	def alphas(self,theta):
+		alpha = np.pi/2.
+		alpha += -np.arcsin(np.cos(theta)/np.sqrt(5.-4*np.sin(theta)))
+		alpha += -np.arcsin(2*np.sqrt(1-np.sin(theta))/np.sqrt(5.-4*np.sin(theta)))
+		return alpha
+	def string(self,thetai,thetaj,debug=False):
+		ai=self.alphas(thetaj)
+		aj=self.alphas(thetai)
+		ti=thetai
+		tj=thetaj
+		R=self.Ro
+		if ai<=ti:
+			#T1 is not used
+			if aj<=tj:
+				#T2 is not used
+				L=R*np.sqrt(pow(2-np.sin(tj)-np.sin(ti),2) + pow(np.cos(tj)-np.cos(ti),2))
+			else:
+				#T2 is used
+				L =R*np.sqrt(pow(2-np.sin(aj)-np.sin(ti),2) + pow(np.cos(aj)-np.cos(ti),2))
+				L+=R*(aj-tj)
+		else:
+			#T1 is used
+			if aj<=tj:
+				#T2 is not used
+				L =R*(ai-ti)
+				L+=R*np.sqrt(pow(2-np.sin(tj)-np.sin(ai),2) + pow(np.cos(tj)-np.cos(ai),2))
+			else:
+				#T2 is used
+				L =R*(ai-ti)
+				L+=R*np.sqrt(pow(2-np.sin(aj)-np.sin(ai),2) + pow(np.cos(aj)-np.cos(ai),2))
+				L+=R*(aj-tj)
+		if debug:
+			print("------------------------")
+			print("    Theta i:  %.6f"%ti)
+			print("    Alpha i:  %.6f"%ai)
+			print("    Theta j:  %.6f"%tj)
+			print("    Alpha i:  %.6f"%aj)
+			print("    Length:   %.6f"%L)
+			print("------------------------")
+		return L
+
+	def factor(self,i,j,debug=False):
+		ti=max(self.thetas[i]-self.dt/2.,0.)
+		ti1=min(self.thetas[i]+self.dt/2.,np.pi/2.)
+		tj=max(self.thetas[j]-self.dt/2.,0.)
+		tj1=min(self.thetas[j]+self.dt/2.,np.pi/2.)
+		s1=self.string(ti,tj,debug=debug)
+		d1=self.string(ti,tj1,debug=debug)
+		d2=self.string(ti1,tj,debug=debug)
+		s2=self.string(ti1,tj1,debug=debug)
+		num=d1+d2-s1-s2
+		den=2*self.Ro*(ti1-ti)
+		if ti>=np.pi/2 or tj>=np.pi/2:
+			F=0.0
+		else:
+			F=num/den
+		if debug:
+			print("------------------------")
+			print("    Sum of sides: %.6f"%(s1+s2))
+			print("    Sum of diags: %.6f"%(d1+d2))
+			print("    Delta theta:  %.6f"%(ti1-ti))
+			print("    View factor:  %.6e"%(F))
+			print("------------------------")
+		return F
+
+	def aperture(self,i,debug=False,inverse=False):
+		ti=max(self.thetas[i]-self.dt/2.,0.)
+		ti1=min(self.thetas[i]+self.dt/2.,np.pi/2.)
+		ai=self.alphas(ti)
+		ai1=self.alphas(ti1)
+		s1=self.Ro*ti
+		d1=self.Ro*np.sqrt(pow(2-np.sin(ai)-np.sin(ti),2)+pow(np.cos(ai)-np.cos(ti),2)) + self.Ro*ai
+		d2=self.Ro*ti1
+		s2=self.Ro*np.sqrt(pow(2-np.sin(ai1)-np.sin(ti1),2)+pow(np.cos(ai1)-np.cos(ti1),2)) + self.Ro*ai1
+		num=d1+d2-s1-s2
+		den=2*self.Ro*(ti1-ti)
+		if self.thetas[i]>=np.pi/2:
+			F=0.0
+		else:
+			if inverse:
+				F=(ti1-ti)/2*num/den
+			else:
+				F=num/den
+		if debug:
+			print("------------------------")
+			print("    Theta i:  %.6f"%ti)
+			print("    Alpha i:  %.6f"%ai)
+			print("    Theta i1: %.6f"%ti1)
+			print("    Alpha i1: %.6f"%ai1)
+			print("    s1:       %.6f"%s1)
+			print("    d1:       %.6f"%d1)
+			print("    d2:       %.6f"%d2)
+			print("    s1:       %.6f"%s2)
+			print("    Sum of sides: %.6f"%(s1+s2))
+			print("    Sum of diags: %.6f"%(d1+d2))
+			print("    View factor:  %.6f"%(F))
+			print("------------------------")
+		return F
+
+	def factors(self,debug=False):
+		F=np.zeros((self.nt+1,self.nt+1))
+		for i in range(self.nt):
+			for j in range(self.nt):
+				F[i,j]=self.factor(i-1,j-1)
+				if i==0 and j>0:
+					F[i,j]=self.aperture(j-1,inverse=True)
+			if i>0:
+				F[i,0]=self.aperture(i-1)
+		if debug:
+			csv = np.sum(F,axis=1)
+			F=np.c_[F,csv]
+			np.savetxt("Factors.csv",F,delimiter=",")
+		return F
 
 	def import_mat(self,fileName):
 		mat = scipy.io.loadmat(fileName, chars_as_strings=False)
@@ -447,6 +567,16 @@ class receiver_cyl:
 		Qnet[net_zero] = 0.0
 		_qnet[net_zero,:] = 0.0
 		self.qnet = _qnet
+
+		q_abs = self.ab*qabs
+		q_abs = np.concatenate((np.flip(q_abs[:,1:-1],axis=1),q_abs),axis=1)
+		self.Q_abs = q_abs.sum(axis=1)*self.Ro*self.dt*self.dz
+		q_rad = self.em*self.sigma*(pow(To,4)-pow(Tamb,4))
+		q_rad = np.concatenate((np.flip(q_rad[:,1:-1],axis=1),q_rad),axis=1)
+		self.Q_rad = q_rad.sum(axis=1)*self.Ro*self.dt*self.dz
+		q_cnv = h_ext*(To - Tamb)
+		q_cnv = np.concatenate((np.flip(q_cnv[:,1:-1],axis=1),q_cnv),axis=1)
+		self.Q_cnv = q_cnv.sum(axis=1)*self.Ro*self.dt*self.dz
 
 		for t in range(Ti.shape[0]):
 			BDp = self.Fourier(Ti[t,:])
